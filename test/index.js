ea91a1d841ae4533a1396ff4633f287c95006121
TJ Holowaychuk <tj@vision-media.ca>
2012-09-04 16:25:30 -0700

    add initial implementation

--- test/index.js
+++ test/index.js
@@ -8,6 +8,6 @@ var mm = require('..');
 describe('{{id}}', function(){
   it('should buffer', function(){
     var user = { name: 'tobi' };
-    mm('hi {{name}}', user).should.equal('hi tobi');
+    mm('hi {{name}}.', user).should.equal('hi tobi.');
   })
 })
