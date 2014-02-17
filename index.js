8ca1a331dc82869fdbcb12e5390a6b7cd79084f0
Ivan Miric <imiric@gmail.com>
2014-02-17 19:05:47 +0100

    Added array support

--- index.js
+++ index.js
@@ -35,46 +35,95 @@ function render(str, obj) {
  */
 
 function compile(str) {
-  var js = [];
-  var toks = parse(str);
-  var tok;
+  var tokens = parse(str);
+  var js = [],
+      sectionLevel = 0,
+      tok;
+  js.push('var out = "";');
 
-  for (var i = 0; i < toks.length; ++i) {
-    tok = toks[i];
+  function prependComma(val) {
+    var jsLast = js[js.length - 1];
+    if (!(jsLast[jsLast.length - 1] === '[')) {
+      // This is not the first element in the list, so prepend a comma
+       val = ', ' + val;
+    }
+    return val;
+  }
+
+  for (var i = 0; i < tokens.length; ++i) {
+    tok = tokens[i];
     if (i % 2 == 0) {
-      js.push('"' + tok.replace(/"/g, '\\"') + '"');
+      tok = tok.replace(/"/g, '\\"');
+      if (tok) {
+        var val = '"' + tok + '"';
+        if (sectionLevel > 0) {
+          js.push(prependComma(val));
+        } else {
+          js.push('out += ' + val + ';');
+        }
+      }
     } else {
       switch (tok[0]) {
-        case '/':
-          tok = tok.slice(1);
-          js.push(') + ');
-          break;
-        case '^':
-          tok = tok.slice(1);
-          assertProperty(tok);
-          js.push(' + section(obj, "' + tok + '", true, ');
-          break;
         case '#':
           tok = tok.slice(1);
           assertProperty(tok);
-          js.push(' + section(obj, "' + tok + '", false, ');
+          var val = 'section(obj, "' + tok + '", false, [';
+          if (sectionLevel > 0) {
+            js.push(prependComma(val));
+          } else {
+            js.push('out += ' + val);
+          }
+          sectionLevel++;
           break;
         case '!':
           tok = tok.slice(1);
           assertProperty(tok);
-          js.push(' + obj.' + tok + ' + ');
+          var val = 'obj.' + tok;
+          if (sectionLevel > 0) {
+            js.push(prependComma(val));
+          } else {
+            js.push('out += ' + val + ';');
+          }
+          break;
+        case '^':
+          tok = tok.slice(1);
+          assertProperty(tok);
+          var val = 'section(obj, "' + tok + '", true, [';
+          if (sectionLevel > 0) {
+            js.push(prependComma(val));
+          } else {
+            js.push('out += ' + val);
+          }
+          sectionLevel++;
+          break;
+        case '/':
+          tok = tok.slice(1);
+          if (sectionLevel > 0) {
+            js.push('])');
+            if (sectionLevel == 1) {
+              js.push(';');
+            }
+          }
+          sectionLevel--;
           break;
         default:
           assertProperty(tok);
-          js.push(' + escape(obj.' + tok + ') + ');
+          if (sectionLevel > 0) {
+            // The bracket indicates it's a property that should be looked up
+            var val = '"{' + tok + '"';
+            js.push(prependComma(val));
+          } else {
+            js.push('out += escape(obj.' + tok + ');');
+          }
       }
     }
   }
+  js.push('return out;');
 
   js = '\n'
     + indent(escape.toString()) + ';\n\n'
     + indent(section.toString()) + ';\n\n'
-    + '  return ' + js.join('').replace(/\n/g, '\\n');
+    + js.join('').replace(/\n/g, '\\n');
 
   return new Function('obj', js);
 }
@@ -124,11 +173,32 @@ function indent(str) {
  * @api private
  */
 
-function section(obj, prop, negate, str) {
+function section(obj, prop, negate, tokens) {
   var val = obj[prop];
-  if ('function' == typeof val) return val.call(obj, str);
+
+  function processTokens(ob, toks) {
+    var out = '';
+    for (var i = 0; i < toks.length; ++i) {
+      var tok = toks[i];
+      if (tok[0] == '{') {
+        out += ob[tok.slice(1)];
+      } else {
+        out += tok;
+      }
+    }
+    return out;
+  }
+
+  if (Array.isArray(val)) {
+    var out = '';
+    for (var i = 0; i < val.length; ++i) {
+      out += processTokens(val[i], tokens);
+    }
+    return out;
+  }
+  if ('function' == typeof val) return val.call(obj, tokens.join(''));
   if (negate) val = !val;
-  if (val) return str;
+  if (val) return processTokens(obj, tokens);
   return '';
 }
 
